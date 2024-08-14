50517c90a0c01bd5db672bea3dcba5cb2ffa1a46
liverpool <tsagpat@liverpools-MacBook-Pro.local>
2024-08-14 02:08:06 -0400

    init youtube transcription summarize

---
+++ src/youtubetranscript/main.py
@@ -0,0 +1,24 @@
+import os
+from openai import OpenAI
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import JSONFormatter
+
+
+# Initial the api call
+client = OpenAI()
+client.api_key(os.getenv("OPENAI_API_KEY"))
+
+# YouTubeTranscriptApi to download the video transcript
+transcripts = YouTubeTranscriptApi.get_transcript("CMsuDp_DER4")
+
+# filter the content 
+content = " ".join([segment['text'] for segment in transcripts  if '[Music]' not in segment['text']] )
+
+# Calling openAI to summary the transcript
+# using model gpt-3.5-turbo
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": f"Summarize the video ranscript:\n{content}"}]
+)
+
+print(response)
